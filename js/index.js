04be04ad6355eb21a60ce231b8b00bda29620f3a
edgar165 <lifestyleedgar754@gmail.com>
2025-08-24 01:13:45 -0400

    commited

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 const arrowLeft = document.querySelector('.icon-angle-left');
 const arrowRight = document.querySelector ('.icon-angle-right');
 
+hello(goodbye);
+
+function hello(callback){
+    console.log("Hello!");
+    callback();
+}
+
+function goodbye(){
+    console.log("Goodbye!");}
